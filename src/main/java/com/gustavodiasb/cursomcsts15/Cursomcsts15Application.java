2ce492fac82854e76d5b68fc93e0ff9a435a8349
Gustavo Dias <blasenbauer.gustavo@gmail.com>
2018-09-18 23:59:55 -0300

    Projeto criado

---
+++ src/main/java/com/gustavodiasb/cursomcsts15/Cursomcsts15Application.java
@@ -0,0 +1,12 @@
+package com.gustavodiasb.cursomcsts15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomcsts15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomcsts15Application.class, args);
+	}
+}

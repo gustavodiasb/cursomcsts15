1b459e6472aeafd5e840082f14888cd408ab9c9f
Gustavo Dias <blasenbauer.gustavo@gmail.com>
2018-09-20 16:58:38 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gustavodiasb/cursomcsts15/services/CategoriaService.java
+++ src/main/java/com/gustavodiasb/cursomcsts15/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.gustavodiasb.cursomcsts15.domain.Categoria;
 import com.gustavodiasb.cursomcsts15.repositories.CategoriaRepository;
+import com.gustavodiasb.cursomcsts15.services.exceptions.ObjectNotFoundException;
 
 
 /* CAMADA DE SERVIÇO */
@@ -17,6 +18,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 
